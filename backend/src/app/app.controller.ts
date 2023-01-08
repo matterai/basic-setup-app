f4af9aed3b9be2e55e4c7cbaf2e7c5d146be9684
matterai <matterai.net@gmail.com>
2023-01-08 11:47:28 -0300

    Model described

--- backend/src/app/app.controller.ts
+++ backend/src/app/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiBody } from '@nestjs/swagger';
 import { AppService } from './app.service';
+import { RegisterModel } from './models/register.model';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @ApiBody({ type: RegisterModel })
+  @Post()
+  register(@Body() dto: RegisterModel) {
+    return dto;
   }
 }
--- backend/src/app/models/register.model.ts
+++ backend/src/app/models/register.model.ts
@@ -1,8 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class RegisterModel {
+  @ApiProperty({
+    description: 'Users email',
+  })
   @IsEmail()
   email: string;
+
+  @ApiProperty({
+    description: 'Reason why user is interesting of product',
+  })
   @IsNotEmpty()
   reason: string;
 }
